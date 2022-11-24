92634ebe61b0c85c38ea29502fb91560c5bc1a1f
vergaraldvm <leonardo11vergara11@gmail.com>
2022-11-24 01:06:46 -0500

    asdas

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/Barranquilla', (_, res) => res.status(200).send('Atlántico'));
+app.get('/barranquilla', (_, res) => res.status(200).send('Atlántico'));
 
-app.get('/Santa Marta', (_, res) => res.status(200).send('Magdalena'));
+app.get('/santamarta', (_, res) => res.status(200).send('Magdalena'));
 
-app.get('/Cartagena', (_, res) => res.status(200).send('Bolívar'));
+app.get('/cartagena', (_, res) => res.status(200).send('Bolívar'));
 
-app.get('/Montería', (_, res) => res.status(200).send('Córdoba'));
+app.get('/bogota', (_, res) => res.status(200).send('Cundinamarca'));
 
-app.get('/Sincelejo', (_, res) => res.status(200).send('Sucre'));
-
-app.use((_, res) => res.status(404).send('Mamaguevo'));
+app.use((_, res) => res.status(404).send('N/A'));
 
 app.listen(app.get('port'), () => {
     console.log('App listening on port', app.get('port'));
